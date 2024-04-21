6648f9c51e105e7e4e0dab45ce108671ef2b1b45
Omkar j <107001176+omkarjninja@users.noreply.github.com>
2024-04-22 02:07:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+let express = require("express");
+let mongoose = require("mongoose");
+let cors = require("cors");
+let bodyParser = require("body-parser");
+
+// Express Route
+const studentRoute = require("./routes/user.routes");
+
+// Connecting mongoDB Database
+mongoose
+  .connect(
+    "mongodb+srv://ojadhav250:OIyYq8zGJvKK7nLF@cluster1.wz4vdqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+  )
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err.reason);
+  });
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+app.use("/students", studentRoute);
+
+// PORT
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log("Connected to port " + port);
+});
+
+// 404 Error
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
+
+// app.post("/login", (req, res) => {
+//   const { email, password } = req.body;
+//   User.findone({ email: email }, (err, user) => {
+//     if (user) {
+//       if (password === user.password) {
+//         res.send({ message: "login sucess", user: user });
+//       } else {
+//         res.send({ message: "wrong credentials" });
+//       }
+//     } else {
+//       res.send("not register");
+//     }
+//   });
+// });
---
+++ routes/user.routes.js
@@ -0,0 +1,97 @@
+let mongoose = require("mongoose"),
+  express = require("express"),
+  router = express.Router();
+
+let user = require("../models/user-schema");
+
+router.route("/create").post(async (req, res, next) => {
+  await user
+    .create(req.body)
+    .then((result) => {
+      res.json({
+        data: result,
+        message: "Data added!",
+        status: 200,
+      });
+    })
+    .catch((err) => {
+      return next(err);
+    });
+});
+
+// router.route("/login", (req, res) => {
+//   const { email, password } = req.body;
+//   User.findone({ email: email }, (err, user) => {
+//     if (user) {
+//       if (password === user.password) {
+//         res.send({ message: "login sucess", user: user });
+//       } else {
+//         res.send({ message: "wrong credentials" });
+//       }
+//     } else {
+//       res.send("not register");
+//     }
+//   });
+// });
+
+router.route("/").get(async (req, res, next) => {
+  await user
+    .find()
+    .then((result) => {
+      res.json({
+        data: result,
+        message: "Data successfully fetched.",
+        status: 200,
+      });
+    })
+    .catch((err) => {
+      return next(err);
+    });
+});
+
+router.route("/edit/:id").get(async (req, res, next) => {
+  await user
+    .findById(req.params.id)
+    .then((result) => {
+      res.json({
+        data: result,
+        message: "Data successfully fetched.",
+        status: 200,
+      });
+    })
+    .catch((err) => {
+      return next(err);
+    });
+});
+
+router.route("/update/:id").put(async (req, res, next) => {
+  await user
+    .findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    })
+    .then((result) => {
+      console.log(result);
+      res.json({
+        data: result,
+        msg: "Data successfully updated.",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.route("/delete/:id").delete(async (req, res, next) => {
+  await user
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.json({
+        msg: "Data successfully updated.",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
